881bef726c9ce45d4875454372764ba4274eabb5
viacaelestis <affectuminflamma@gmail.com>
2023-03-06 02:33:32 -0700

    do more query practice with join

---
+++ 2_queries_joins/cohort_duration.sql
@@ -0,0 +1,15 @@
+-- Get the total amount of time that all students from a specific cohort have spent on all assignments.
+
+-- This should work for any cohort but use FEB12 for now.
+-- Select only the total amount of time as a single column.
+-- You can write as many JOIN statements as you need in a single query. Expected Result:
+--  total_duration 
+-- ----------------
+--          373501
+-- (1 row)
+
+SELECT sum(assignment_submissions.duration) as total_duration
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = 'FEB12';
